3b8a191f05ffd67362745dc1164f7ab7d55dd625
Anthony <bonsilaoanthony@gmail.com>
2022-07-25 13:05:33 -0400

    Starting files added

---
+++ Wk13_GuidedActivity/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper
+		{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2
+		{
+            margin-top: 0;
+        }
+        table tr td:last-child a
+		{
+            margin-right: 15px;
+        }
+    </style>
+	<!--include a simple Bootstrap plugin-->
+	<!--this is a small pop-up box that appears when the user moves the mouse pointer over an element-->
+    <script type="text/javascript">
+       
+    </script>
+</head>
+<body>
+<div class="wrapper">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<div class="page-header clearfix">
+<h2 class="pull-left">Employees Details</h2>
+   <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+</div>
+
+<?php
+// Include config file
+
+                    
+// Attempt select query execution
+
+// Create the table so the records are easily displayed	
+
+// Create a loop to process the records and then populate the table
+
+
+// Free result set
+
+ 
+// Close connection
+
+?>
+</div>
+</div>        
+</div>
+</div>
+</body></html>
